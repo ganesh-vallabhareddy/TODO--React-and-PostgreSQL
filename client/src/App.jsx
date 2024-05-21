daef064ead71cb1cd0332505c8a1d02bdee7fd7b
vallabhareddy-ganesh <vallabhareddyganesh@gmail.com>
2024-05-21 20:34:01 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React,{Fragment} from 'react'
+import './App.css'
+import InputTodo from './components/InputTodo'
+import ListTodo from './components/ListTodo'
+
+
+function App() {
+  
+
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo></InputTodo>
+        <ListTodo></ListTodo>
+      </div>
+
+    </Fragment>
+  )
+}
+
+export default App
